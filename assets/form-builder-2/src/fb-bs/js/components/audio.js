531a293691b795fd03fa82500f5e976e18488290
yolo <kuroikenshi@sina.com>
2017-12-27 17:11:30 +0800

    修改fb-bs中的audio，完成audio的上传功能

--- assets/form-builder-2/src/fb-bs/js/components/audio.js
+++ assets/form-builder-2/src/fb-bs/js/components/audio.js
@@ -29,6 +29,18 @@
 						'<button type="button" class="btn btn-danger delete"><i class="fa fa-close"></i></button>' +
 					'</span>' +
 				'</span>';
+		this.recorderTemplate =
+				'<div class="recorder">' +
+					'<span class="record-btn-part">' +
+						'<button class="btn btn-default record-btn" type="button"><i class="fa fa-circle"></i><span>&nbsp;REC</span></button>' +
+					'</span>' +
+					'<span class="test-btn-part hide" style="margin-left: 8px;">' +
+						'<audio class="fb-audio" controls="controls"></audio>' +
+						'<button class="btn btn-default upload-btn" type="button" style="margin-left: 8px;"><i class="fa fa-upload"></i><span></span></button>' +
+						'<button class="btn btn-danger reset-btn" type="button" style="margin-left: 8px;"><i class="fa fa-close"></i><span></span></button>' +
+					'</span>' +
+					'<p class="help-info form-control-static" style="padding-left: 9px;"></p>' +
+				'</div>';
 
 		var that = this ;
 
@@ -40,8 +52,85 @@
 				that.setValue(value);
 			});
 
-			// TODO: 渲染录音部分(未完成)
-			this.$node.find('.recorder-placeholder').renderRecorder();
+			// 渲染录音部分
+			// this.$node.find('.recorder-placeholder').renderRecorder();
+			var $recorderNode = $(this.recorderTemplate);
+			this.$node.find('.recorder-placeholder').replaceWith($recorderNode);
+
+			var recorder;
+
+			var audio = $recorderNode[0].querySelector('audio');
+
+			/*// 结束监听
+			audio.addEventListener('ended', function() {
+				$recorderNode.find('.test-btn .playIcon').addClass('fa-play').removeClass('fa-stop');
+			}, false);*/
+
+			function startRecording() {
+				HZRecorder.get(function (rec) {
+					recorder = rec;
+					recorder.start();
+				});
+			}
+			function stopRecording() {
+				recorder.stop();
+				recorder.play(audio);
+
+				console.log('当前录音值为:', recorder.getBlob());
+			}
+
+			console.log($recorderNode.find('.record-btn').toggle);
+
+			// 录音按钮事件绑定
+			$recorderNode.find('.record-btn').on('click', function() {
+				that.$node.find('.help-info').html('');
+				var $recordIcon = $recorderNode.find('.record-btn .fa-circle');
+				if ($recordIcon.hasClass('active')) {
+					// 结束录音
+					stopRecording();
+					$recordIcon.removeClass('active');
+
+					// 显示预览部分
+					$recorderNode.find('.test-btn-part').removeClass('hide');
+				} else {
+					// 开始录音
+					startRecording();
+					$recordIcon.addClass('active');
+
+					// 隐藏预览部分
+					$recorderNode.find('.test-btn-part').addClass('hide');
+				}
+			});
+
+			// 上传按钮的绑定
+			$recorderNode.find('.upload-btn').on('click', function() {
+				var opt = {
+					data: {
+						file: recorder.getBlob(),
+						key: 'testUploadAudio.wav'
+					},
+					success: function(res) {
+						console.log('upload success');
+						var domain = $('#qiniuDomain').val();
+						that.setValue("audios:[" + domain + res.key + "]");
+					},
+					error: function(res) {
+						console.log('upload error.', res);
+						that.$node.find('.help-info').html($.parseJSON(res.responseText)['error']);
+					}
+				};
+
+				qiniu_upload(opt);
+			});
+
+			// 清空按钮的绑定
+			$recorderNode.find('.reset-btn').on('click', function() {
+				audio.setAttribute('src', null);
+				that.$node.find('.help-info').html('');
+
+				$recorderNode.find('.test-btn-part').addClass('hide');
+			});
+
 
 			// 删除按钮事件绑定
 			this.$node.find('.play-part .delete').on('click', function(e) {
@@ -60,6 +149,8 @@
 			if (this.value.match(/audios:\[(.*)\]/) !== null) {
 				this.$node.find('.play-part').removeClass('hide');
 				this.$node.find('.record-part').addClass('hide');
+				this.$node.find('.test-btn-part').addClass('hide');
+				this.$node.find('.help-info').html('');
 			} else {
 				this.$node.find('.play-part').addClass('hide');
 				this.$node.find('.record-part').removeClass('hide');
@@ -69,20 +160,25 @@
 
 		this.__setValue = function(data) {
 			console.log('audio / this.__setValue("' + data + '")');
-			if(that.opts.isRead) {
 
-                this.$node.find('input').val(data);
-                var url = "";
+            this.$node.find('input').val(data);
+            var url = "";
 
-                if (data && data.match(/audios:\[(.*)\]/) !== null) {
-                    url = data.match(/audios:\[(.*)\]/)[1].split(',')[0];
-                }
-
-                // 给播放部分的audio赋值
-                this.$node.find('.play-part audio').attr('src', url);
+            if (data && data.match(/audios:\[(.*)\]/) !== null) {
+                url = data.match(/audios:\[(.*)\]/)[1].split(',')[0];
             }
-		};
 
+            // 给播放部分的audio赋值
+            console.log('给播放部分的audio赋值，发现' + this.$node.find('.play-part audio').length + '个')
+            this.$node.find('.play-part audio').attr('src', url);
+            console.log('复制完毕后:', this.$node.find('.play-part audio')[0].outerHTML);
+            
+			if (that.opts.isRead) {
+				this.$node.find('.delete').hide();
+			} else {
+				this.$node.find('.delete').show();
+			}
+		};
 
 
 		this.__afterSetValue = function(data) {
---
+++ assets/form-builder-2/src/fb-bs/js/extra/qiniuCustomized.js
@@ -0,0 +1,66 @@
+;(function (factory) {
+	'use strict';
+	if (typeof define === "function" && define.amd) {
+		// AMD模式
+		define(['jquery'], factory);
+	} else {
+		// 全局模式
+		factory(jQuery);
+	}
+}(function ($) {
+	window.qiniu_upload = function(opts) {
+		var formData = new FormData();
+		
+		// TODO: 改成从后台获取token
+
+		formData.append('file', opts.data.file);
+		// 这里需要设置正确的token
+		formData.append('token', 'YpOBLwOzxNPBDAAD9cJaND2xh3TkgtTilQERWHSByAQ_:9CNVM9IegER_FjIbGKw3qcjTvIQ=:eyJzY29wZSI6ImJwbXRlc3QiLCJkZWFkbGluZSI6MTU0OTIxNTMxNX0=');
+		formData.append('key', opts.data.key);
+
+		$.ajax({
+			url: 'http://up-z1.qiniu.com',
+			method: 'POST',
+			data: formData,
+			processData: false,
+			contentType: false,
+			/*xhr: function(){
+				myXhr = $.ajaxSettings.xhr();	 
+				if(myXhr.upload){
+					myXhr.upload.addEventListener('progress',function(e) {
+						// console.log(e);
+						if (e.lengthComputable) {
+							var percent = e.loaded/e.total*100;
+							$progress.html('上传：' + e.loaded + "/" + e.total+" bytes. " + percent.toFixed(2) + "%");
+						}
+					}, false);
+				}
+				return myXhr;
+			},*/
+			beforeSend: function(xhr) {
+				console.log('BEFORESEND', xhr);
+				if (opts.beforeSend) {
+					opts.beforeSend(xhr);
+				}
+			},
+			success: function(res) {
+				console.log('SUCCESS', res);
+				if (opts.success) {
+					opts.success(res);
+				}
+			},
+			error: function(res) {
+				console.log('ERROR', res);
+				if (opts.error) {
+					opts.error(res);
+				}
+			},
+			complete: function(res, ts) {
+				console.log('COMPLETE', res, ts);
+				if (opts.complete) {
+					opts.complete(res, ts);
+				}
+			}
+		});
+	}
+}));
